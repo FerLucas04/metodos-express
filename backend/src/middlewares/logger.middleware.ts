2b2b3c11bf365144c1e0d4923d822646125c8095
Lucas <aquinolucasfernando@gmail.com>
2026-09-18 16:05:02 -0300

    modificaciones en la estructura

---
+++ backend/src/middlewares/logger.middleware.ts
@@ -0,0 +1,12 @@
+import * as Yup from "yup";
+
+export const loginScheme = Yup.object({
+  email: Yup.string()
+    .trim()
+    .email("El correo es inválido")
+    .required("Este campo es obligatorio"),
+  password: Yup.string()
+    .min(6, "La contraseña debe tener al menos 6 caracteres")
+    .max(20, "La contraseña debe tener como máximo 20 caracteres")
+    .required("Este campo es obligatorio"),
+});
